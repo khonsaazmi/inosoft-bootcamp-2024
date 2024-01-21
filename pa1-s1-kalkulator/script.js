09ff3570014502759f606426c3fc7d75d4b39080
khonsaazmi <khonsaazmi@gmail.com>
2024-01-21 20:55:00 +0700

    recommit

--- pa1-s1-kalkulator/script.js
+++ pa1-s1-kalkulator/script.js
@@ -1,28 +1,33 @@
-function insert(num) {
+function insert(num) { // Function to insert numbers on the display
     document.form.display.value = document.form.display.value + num
 }
 
-function back() {
+function back() { // Function to delete one character of the display
     var exp = document.form.display.value;
     document.form.display.value = exp.substring(0, exp.length - 1);
 }
 
-function clean() {
+function clean() { // Function to clear the display
     document.form.display.value = "";
 }
 
-function equal() {
+function equal() { // Function to evaluate the expression
     try {
         const display = document.form.display; // Assuming 'display' is the name of the input field
-        let exp = display.value.replace(/x/g, '*').replace(/÷/g, '/').replace(/%/g, '1/100'); // Replace 'x' with '*' to handle multiplication
+        // Replace 'x' with '*' to handle multiplication
+        // Replace '÷' with '/' to handle division
+        // Replace '%' with '1/100' to handle percentage
+        let exp = display.value.replace(/x/g, '*').replace(/÷/g, '/').replace(/%/g, '1/100'); 
         if (exp) {
             display.value = eval(exp);
         }
+    // Catch any errors
     } catch (error) {
         display.value = "Error";
     }
 }
 
+// SWITCH MODE
 var icon = document.getElementById("light-mode");
 
 icon.onclick = function () {
